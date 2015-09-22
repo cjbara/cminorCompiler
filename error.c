ae4d76c578453452dab42aa3d1e49d19c611c9d5
Cory Jbara <cjbara@nd.edu>
2015-09-22 13:21:29 -0400

    Working Scanner

--- error.c
+++
@@ -1,9 +0,0 @@
-#include "error.h"
-#include "globl.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-void error() {
-  fprintf(stderr,"scan error: %s is not a valid character\n", yytext);
-  exit(1);
-}
--- globl.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-extern int yylex();
-extern char * yytext;
-extern FILE * yyin;
-
