7643f9d873189cd15424b2d20621d60757d072bf
Cory Jbara <cjbara@nd.edu>
2015-11-17 15:42:34 -0500

    Updated scanner

--- scanner/cminor.c
+++ scanner/cminor.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+int scan();
+
 int main(int argc, char *argv[]){
   char* filename;
   char output[512];
@@ -11,15 +13,28 @@ int main(int argc, char *argv[]){
 	filename = argv[2];
 	/* open the file for reading */
 	yyin = fopen(filename, "r");
-	
+
+	/* Scan the file */
+	return scan();
+  } else {
+	return 2;
+  }
+}
+
+//This function scans a file
+int scan(){
+  	char output[512];
+
 	/* Declare the token variable */
 	token_t token;
 
 	/* For each token, scan it */
 	token = yylex();
 	while(token){
+	  //prints out an integer literal
 	  if(token == TOKEN_INTEGER_LITERAL){
 	    printf("%s %s\n", token_string(token), yytext);
+	  //This else statement prints out a string or char literal
 	  } else if(token == TOKEN_STRING_LITERAL || token == TOKEN_CHAR_LITERAL){
 	      int i;
 	      int offset = 1;
@@ -39,21 +54,21 @@ int main(int argc, char *argv[]){
 		}
 		if(i-offset >= 255){
 		  fprintf(stderr,"error: string cannot exceed 255 characters\n");
-		  exit(1);
+		  return 1;
 		}
 	      }
 	      output[i-offset] = '\0';
 	      printf("%s %s\n", token_string(token), output);
+	  //This else prints out an error message if there is a bad scan
 	  } else if(token == TOKEN_ERROR){
 	    error();
 	    return 1;
+	  //This prints out the other categories of tokens
 	  } else {
 	    printf("%s\n", token_string(token));
 	  }
 	  token = yylex();
 	}
-  } else {
-	return 2;
-  }
-  return 0;
+	//scan completed, successfully, return 0
+	return 0;
 }
